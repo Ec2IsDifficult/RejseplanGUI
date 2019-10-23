ab2038472ce3a342217e455e8561bee09acf2553
Ec2IsDifficult <rasser.arm@live.dk>
2019-10-23 14:44:33 +0200

    Added a constraint to the query for filtering out non valid departures. You cant travel back in time, sorry

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -27,7 +27,7 @@ public class Controller {
     @FXML
     ComboBox<String> comboBoxEndTime;
     private ObservableList<String> timeList = FXCollections.observableArrayList("00.00", "00.30", "01.00", "01.30",
-            "02.00", "02.30", "03.00", "03.30", "04.00", "04.30", "05.00", "05.22", "06.00", "06.30", "07.00", "07.30", "08.00", "08.30",
+            "02.00", "02.30", "03.00", "03.30", "04.00", "04.30", "05.00", "05.30", "06.00", "06.30", "07.00", "07.30", "08.00", "08.30",
             "09.00", "09.30", "10.00", "10.30", "11.00", "11.30", "12.00", "12.30", "13.00", "13.30", "14.00", "14.30",
             "15.00", "15.30", "16.00", "16.30", "17.00", "17.30", "18.00", "18.30", "19.00", "19.30", "20.00", "20.30",
             "21.00", "21.30", "22.00", "22.30", "23.00", "23.30");
@@ -55,6 +55,7 @@ public class Controller {
 
         //Assign input from GUI into variables later to be inserted into the PreparedStatement.
         float d = Float.parseFloat(comboBoxEndTime.getValue());
+
         String string1 =  inputFieldStartStation.getText();
         String string2 =  inputFieldEndStation.getText();
 
@@ -71,8 +72,9 @@ public class Controller {
             String resultStartStation = res.getString(2);
             float resultArrivalTime = res.getFloat(3);
             String resultEndStation = res.getString(4);
-            String results = resultStartTime + " " + resultStartStation +  " " +  resultArrivalTime + " " + resultEndStation;
-            textArea.appendText(results);
+            String results = "Tag toget klokken: " + resultStartTime + " \nfra stationen " + resultStartStation +
+                    " \n" + "Du ankommer klokken: " + resultArrivalTime + " \npå " + resultEndStation + " \n";
+            textArea.appendText(results + "\n");
         }
 
         //Close resultset and connection.
@@ -91,7 +93,8 @@ public class Controller {
                 "Stations AS sar ON ar.StationID = sar.StationID " +
                 "INNER JOIN " +
                 "Stations AS sds ON ds.StationID = sds.StationID " +
-                "WHERE sar.StationName = '"+ EndStationName+ "'" + "And ar.StartTime = '" + arrivalTime + "'" + "And sds.StationName = '" + StartStationName + "'";
+                "WHERE sar.StationName = '"+ EndStationName+ "'" + "And ar.StartTime <= '" + arrivalTime + "'" + "And ds.StartTime <= ar.StartTime " +
+                "And sds.StationName = '" + StartStationName + "'";
 
         departureFromStart = conn.prepareStatement(query);
         return departureFromStart;
